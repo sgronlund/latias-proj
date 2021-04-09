c77da955da85e56a2e25a2ea3a3549d6959603bd
aigu6626 <aigu6626@student.uu.se>
2021-04-09 09:46:02 +0200

    stylesheet and menu

---
+++ src/screens/Menu.js
@@ -0,0 +1,17 @@
+import { StatusBar } from 'expo-status-bar';
+import React, { useState} from 'react';
+import {styleSheets} from "../styleSheets/StyleSheets";
+import { StyleSheet, Text, View, Button, TextInput, SafeAreaView } from 'react-native';
+import socketClient, { io }  from "socket.io-client";
+//kolla upp props
+function Menu(props){
+    return (
+        <View style = {styleSheets.background}>
+           <View style = {styleSheets.boarder}>
+        </View>
+        </View>
+       
+        );
+}
+
+export default Menu;
