ef8ff1cadf96a5967a9a3ba96b213c32ce7412ad
aigu6626 <aigu6626@student.uu.se>
2021-04-09 09:46:02 +0200

    stylesheet and menu

--- src/App.js
+++ src/App.js
@@ -1,66 +1,8 @@
-import { StatusBar } from 'expo-status-bar';
-import React, { useState} from 'react';
-import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
-import socketClient, { io }  from "socket.io-client";
+import * as React from "react";
+import Menu from "./screens/Menu";
 
-export default function App() {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  /**
-   * When the button is pressed we emit the message btnpress over our connected socket.
-   * This emission must be handled in the server by using a socket.on('btnpress', ...),
-   * which lets us define a behaviour for the server when it receives this message
-   */
-  return (
-    <View style={styles.container}>
-      <TextInput
-        style={{height: 40}}
-        placeholder="Username"
-        onChangeText={username => setUsername(username)} //Can do this in Register/Login function
-      />
-      <TextInput
-        style={{height: 40}}
-        placeholder="Password"
-        onChangeText={password => setPassword(password)}
-      />
-      <Button title="Register" onPress = {() => {Register(username, password)}}/>
-      <Button title="Login" onPress = {() => {Login(username, password)}}/>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const App = () => {
+  return <Menu />;
+};
 
-// Function that initializes sockets
-function initSockets(socket) {
-  socket.on('connection', () => {console.log(`I'm connected with the back-end`);});
-  socket.on('success', () => {console.log("logged in")/*TODO*/});
-  socket.on('failure', () => {console.log("fail")/*TODO*/});
-}
-
-// Registers the user in the database
-function Register(username, password) {
-  var socket = Connect();
-  socket.emit('register', username, password);
-}
-
-// Logs in the user
-function Login(username, password) {
-  var socket = Connect();
-  socket.emit('login', username, password);
-}
-
-// Connects the user to the server
-function Connect() {
-  var socket = socketClient ("http://192.168.1.150:8080");
-  initSockets(socket);
-  return socket;
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
---
+++ src/styleSheets/StyleSheets.js
@@ -0,0 +1,17 @@
+import {StyleSheet} from "react-native";
+
+
+
+
+export const styleSheets = StyleSheet.create({
+    background: {
+        flex: 1,
+        backgroundColor: "#383E6E",
+
+    },
+    boarder: {
+        width: 100 * 5,
+        height: 100,
+        backgroundColor: "#545985",
+    },
+});
