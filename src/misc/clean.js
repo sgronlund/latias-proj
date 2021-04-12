a4e3c8a0b2fcc0d00ee8eb683dd6531be4cbe137
JakobPaulsson <JakobPaulsson@users.noreply.github.com>
2021-04-12 14:01:42 +0000

    Formatted code using prettier

--- src/misc/clean.js
+++ src/misc/clean.js
@@ -1,8 +1,10 @@
-var os = require('os');
-const { exec } = require('child_process');
+var os = require("os");
+const { exec } = require("child_process");
 
-if(os.type() === "Windows_NT") {
-  exec('del package-lock.json && rd /s /q \"../docs/out\" && rd /s /q node_modules');
-} else if(os.type() === "Darwin" || os.type() == "Linux") {
-  exec('rm -f package-lock.json && rm -rf node_modules && rm -rf ../docs/out');
+if (os.type() === "Windows_NT") {
+  exec(
+    'del package-lock.json && rd /s /q "../docs/out" && rd /s /q node_modules'
+  );
+} else if (os.type() === "Darwin" || os.type() == "Linux") {
+  exec("rm -f package-lock.json && rm -rf node_modules && rm -rf ../docs/out");
 }
