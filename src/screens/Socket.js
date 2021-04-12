6edddf104540609abbfd72c8ffab86c1a4837e81
JakobPaulsson <JakobPaulsson@DESKTOP-RT1RHK6>
2021-04-12 16:00:18 +0200

    Forgot imports

--- src/screens/Socket.js
+++
@@ -1,8 +0,0 @@
-import socketClient from "socket.io-client";
-
-/**
- * @summary Connects a socket to the given ip
- */
-const Socket = socketClient("http://localhost:8080");
-
-export default Socket;
