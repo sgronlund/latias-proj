364bae4e93f26ff19767c232f09df4f4d4c56a01
niclasgardsuu <niclasgards@hotmail.com>
2021-04-14 18:29:49 +0200

    camelcase socket listeners, and removed some testing logs

--- src/misc/Socket.js
+++ src/misc/Socket.js
@@ -11,24 +11,17 @@ const Socket = socketClient("http://localhost:8080");
  */
 let sharedKey;
 
-Socket.on('server-public', (server_public,g,p) => {
+Socket.on('serverPublic', (server_public,g,p) => {
       server_public = bigInt(server_public);
       g = bigInt(g);
       p = bigInt(p);
-      console.log(g);
       var private_key = bigInt(1337420); //TODO generera på annat sätt
       var my_public_key = g.modPow(private_key,p);
       sharedKey = server_public.modPow(private_key,p);
 
-      if(sharedKey == undefined) {
-          sharedKey.wtfsyntaxkek();
-      }
-
-      console.log(sharedKey + " yes it worked wowowoowowowoow");
-
-      Socket.emit('client-public', Number(my_public_key));
+      Socket.emit('clientPublic', Number(my_public_key));
     });
-Socket.emit('start-key-exchange');
+Socket.emit('startKeyExchange');
 
 export default Socket;
 export {sharedKey, Socket};
