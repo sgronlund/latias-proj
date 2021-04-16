c8f9d920f48fa422fe0b9ecb98dc101b8b0c199c
Sebastian Grönlund <sebastian.gronlund.1117@student.uu.se>
2021-04-16 16:28:51 +0200

    Added tests for react navigation

--- src/jest/setup.js
+++ src/jest/setup.js
@@ -7,3 +7,15 @@ jest.mock("react-navigation", () => ({
   ),
   SafeAreaView: ({ children }) => <>{children}</>,
 }));
+
+jest.mock('react-native/Libraries/Animated/src/NativeAnimatedHelper');
+
+jest.mock('react-native-reanimated', () => {
+  const Reanimated = require('react-native-reanimated/mock');
+
+  // The mock for `call` immediately calls the callback which is incorrect
+  // So we override it with a no-op
+  Reanimated.default.call = () => {};
+
+  return Reanimated;
+});
