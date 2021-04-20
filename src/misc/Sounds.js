6865568f65d695b2a2673a356d506b238a320ca3
JakobPaulsson <JakobPaulsson@users.noreply.github.com>
2021-04-20 09:18:20 +0000

    Formatted code using prettier

--- src/misc/Sounds.js
+++ src/misc/Sounds.js
@@ -1,17 +1,14 @@
-import { Audio } from 'expo-av';
+import { Audio } from "expo-av";
 
 let loadButtonSound = async () => {
   const { sound } = await Audio.Sound.createAsync(
-     require('../assets/sounds/button.mp3')
+    require("../assets/sounds/button.mp3")
   );
   return sound;
-}
+};
 
 let playButtonSound = (sound) => {
   sound.playAsync();
-}
-
-export {
-  loadButtonSound,
-  playButtonSound
 };
+
+export { loadButtonSound, playButtonSound };
