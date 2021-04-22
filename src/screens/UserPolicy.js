fee552f26ec47e34555abf1f0347c20ced5349d1
JakobPaulsson <JakobPaulsson@users.noreply.github.com>
2021-04-22 17:23:44 +0000

    Formatted code using prettier

--- src/screens/UserPolicy.js
+++ src/screens/UserPolicy.js
@@ -1,10 +1,5 @@
 import React from "react";
-import {
-  SafeAreaView,
-  StyleSheet,
-  Text,
-  ScrollView
-} from "react-native";
+import { SafeAreaView, StyleSheet, Text, ScrollView } from "react-native";
 import theme from "../styles/themes";
 import styleSheets from "../styles/StyleSheets";
 
@@ -21,20 +16,19 @@ class UserPolicy extends React.Component {
           <Text style={styles.Text}>
             {/*FIXME: Add actual relevant content*/}
             "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-            minim veniam, "
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, "
           </Text>
           <Text style={styles.Text}>
             {"\n"}
             {/*FIXME: Add actual relevant content*/}
-            quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum."
+            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+            commodo consequat. Duis aute irure dolor in reprehenderit in
+            voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
+            officia deserunt mollit anim id est laborum."
           </Text>
         </ScrollView>
-        
       </SafeAreaView>
     );
   }
@@ -51,7 +45,7 @@ const styles = StyleSheet.create({
   Text: {
     fontFamily: "Roboto Slab",
     fontSize: theme.FONT_SIZE_EXTRA_SMALL,
-    color: "white"
+    color: "white",
   },
 });
 
