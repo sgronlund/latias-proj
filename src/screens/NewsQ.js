312f50961fd60ca53cf6f89980cf71547a21703e
Sebastian Grönlund <55285451+sgronlund@users.noreply.github.com>
2021-05-07 12:32:18 +0200

    Update src/screens/NewsQ.js
    Co-authored-by: niclasgardsuu <61283210+niclasgardsuu@users.noreply.github.com>

--- src/screens/NewsQ.js
+++ src/screens/NewsQ.js
@@ -375,13 +375,6 @@ const styles = StyleSheet.create({
     margin: theme.MARGIN_SMALL,
     padding: theme.PADDING_SMALL,
   },
-  rowContainer: {
-    flexWrap: "wrap",
-    width: "100%",
-    height: "50%",
-    flexDirection: "row",
-    alignItems: "center",
-  },
   Gradient: {
     flex: 1,
     width: "100%",
